901ca8b2b1d6795013292de1b134dfac074b8f54
Dušan Popov <aristocut90@gmail.com>
2019-08-20 09:34:49 +0200

    rewrote all of the functionalities in vanilla JS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
-$(".hamburger-menu").on("click", function(){
-    $(this).toggleClass("active");
-    $(".overlay").toggleClass("menu-open");
-});
+// $(".hamburger-menu").on("click", function(){
+//     $(this).toggleClass("active");
+//     $(".overlay").toggleClass("menu-open");
+// });
 
-$(".main-navbar__link").on("click", function(){
-    $(".hamburger-menu").removeClass("active");
-    $(".overlay").removeClass("menu-open");
-});
+// $(".main-navbar__link").on("click", function(){
+//     $(".hamburger-menu").removeClass("active");
+//     $(".overlay").removeClass("menu-open");
+// });
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const overlay = document.querySelector(".overlay");
+const mainNavbarLink = document.querySelector(".main-navbar__link");
+
+function toggleHamburgerMenu(){
+    hamburgerMenu.classList.toggle("active");
+    overlay.classList.toggle("menu-open");
+}
+
+function navbarClassRemove(){
+    hamburgerMenu.classList.remove("active");
+    overlay.classList.remove("menu-open");
+}
+
+hamburgerMenu.addEventListener("click", toggleHamburgerMenu);
+mainNavbarLink.addEventListener("click", navbarClassRemove);
