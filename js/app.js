37c36ee8b5bf71464d4f49ddf7de9a33e924f8fe
Dušan Popov <aristocut90@gmail.com>
2021-03-29 07:10:30 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-// $(".hamburger-menu").on("click", function(){
-//     $(this).toggleClass("active");
-//     $(".overlay").toggleClass("menu-open");
-// });
-
-// $(".main-navbar__link").on("click", function(){
-//     $(".hamburger-menu").removeClass("active");
-//     $(".overlay").removeClass("menu-open");
-// });
-
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const overlay = document.querySelector(".overlay");
 const mainNavbarLink = document.querySelector(".main-navbar__link");
